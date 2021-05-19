8400fea27e5aadeede7368ad21897b071c2581d8
GeoMukkath <geomukkath@yandex.com>
2021-05-19 16:06:08 +0530

    Added list items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import React, { useState } from "react";
 import "./App.css";
+import { Button } from "@material-ui/core";
+import Message from "./Message.js";
 
 function App() {
+  const [chats, setChats] = useState(["Hi", "Hello"]);
+  const [input, setInput] = useState("");
+
   return (
     <div>
-      <h1 className="pt-2">Chat Room </h1>
+      <h1 className="pt-2">Chat Room 🚀</h1>
       <div className="d-block w-100">
         <form>
-          <input />
-          <input />
-          <button>Send</button>
+          <input className="mb-2" placeholder="Enter Name" />
+          <input className="mb-2" placeholder="Enter Message" />
+          <Button variant="contained" color="primary">
+            Send
+          </Button>
         </form>
+      </div>
+      <div>
+        <ul>
+          {chats.map((chat) => (
+            <Message text={chat} />
+          ))}
+        </ul>
       </div>
     </div>
   );
---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Message(props) {
+  return (
+    <div>
+      <li>{props.text}</li>
+    </div>
+  );
+}
+
+export default Message;
