f4a98abd8271df596da5c4fdbcbbb81f87167ca9
GeoMukkath <geomukkath@yandex.com>
2021-05-19 16:38:11 +0530

    Sending messages locally successful

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Button } from "@material-ui/core";
 import Message from "./Message.js";
 
 function App() {
   const [chats, setChats] = useState(["Hi", "Hello"]);
+  const [name, setName] = useState("");
   const [input, setInput] = useState("");
 
+  useEffect(() => {}, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setChats([...chats, name, input]);
+  };
   return (
     <div>
       <h1 className="pt-2">Chat Room 🚀</h1>
       <div className="d-block w-100">
         <form>
-          <input className="mb-2" placeholder="Enter Name" />
-          <input className="mb-2" placeholder="Enter Message" />
-          <Button variant="contained" color="primary">
+          <input
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            className="mb-2"
+            placeholder="Enter Name"
+          />
+          <input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            className="mb-2"
+            placeholder="Enter Message"
+          />
+          <Button
+            disabled={!input}
+            onClick={sendMessage}
+            variant="contained"
+            color="primary"
+          >
             Send
           </Button>
         </form>
