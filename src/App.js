c2d23a5aa669d4d9ea09cf25e5050705c698ccd5
GeoMukkath <geomukkath@yandex.com>
2021-05-19 15:14:25 +0530

    Added form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div>Chat Room</div>;
+  return (
+    <div>
+      <h1 className="pt-2">Chat Room </h1>
+      <div className="d-block w-100">
+        <form>
+          <input />
+          <input />
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
